0ebf14c6ad9109260eb841d4700000563c080671
Pouya SadatAlhosseini <pouya.s2781@gmail.com>
2025-02-17 15:56:11 +0330

    test

--- index.js
+++ index.js
@@ -26,48 +26,16 @@ function writeFileRelative(relativePath, data) {
     });
 }
 
-function convertCoverageDataToLineArray(data) {
-    return data.split('\n');
-}
-
-function normalizeCoverageDataLines(dataLines) {
-    if (dataLines.length <= 2) {
-        return [];
-    }
-    return dataLines.slice(1, dataLines.length - 1);
-}
-
-function mergeNormalizedCoverageDataLines(allDataLines) {
-    return allDataLines.flat();
-}
-
-function finalizeCoverageDataLines(dataLines) {
-    if (dataLines.length === 0) {
-        return ['TN:', 'end_of_record'];
-    }
-    return ['TN:', ...dataLines, 'end_of_record'];
-}
-
-function convertCoverageDataLinestoData(dataLines) {
-    return dataLines.join('\r\n');
-}
-
 
 async function main() {
     const coveragetFolderRelativePath = process.argv[2] ?? './coverage';
     const lcovPaths = await globby(`${coveragetFolderRelativePath}/**/**/lcov.info`);
     const lcovValidPaths = lcovPaths.filter(lcovPath => lcovPath !== `${coveragetFolderRelativePath}/lcov.info`);
-    console.log('files', lcovValidPaths)
 
     const coverageDatum = await Promise.all(lcovValidPaths.map(lcovPath => readFileRelative(lcovPath)))
-    console.log('data', coverageDatum);
+    const mergedData = coverageDatum.join('\n');
 
-    const coverageDataLines = coverageDatum.map(coverageData => normalizeCoverageDataLines(convertCoverageDataToLineArray(coverageData)))
-    const mergedCoverageDataLines = mergeNormalizedCoverageDataLines(coverageDataLines);
-    const finalizedCoverageDataLines = finalizeCoverageDataLines(mergedCoverageDataLines);
-    const finalCoverageData = convertCoverageDataLinestoData(finalizedCoverageDataLines);
-
-    writeFileRelative(`${coveragetFolderRelativePath}/lcov.info`, finalCoverageData).catch((error) => {
+    writeFileRelative(`${coveragetFolderRelativePath}/lcov.info`, mergedData).catch((error) => {
         console.error(error);
     })
 }
