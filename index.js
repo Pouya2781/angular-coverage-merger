ddf9754571d12679f003dafddebac2ac2a3b6df9
Pouya SadatAlhosseini <pouya.s2781@gmail.com>
2025-02-17 14:49:45 +0330

    testing

--- index.js
+++ index.js
@@ -1,72 +1,74 @@
 #!/usr/bin/env
-import fs from 'fs'
-import {globby} from 'globby'
+// import fs from 'fs'
+// import {globby} from 'globby'
 
-function readFileRelative(relativePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(relativePath, 'utf8', (error, data) => {
-            if (error) {
-                reject(`Error reading file: ${error.message}`);
-            } else {
-                resolve(data);
-            }
-        });
-    });
-}
+// function readFileRelative(relativePath) {
+//     return new Promise((resolve, reject) => {
+//         fs.readFile(relativePath, 'utf8', (error, data) => {
+//             if (error) {
+//                 reject(`Error reading file: ${error.message}`);
+//             } else {
+//                 resolve(data);
+//             }
+//         });
+//     });
+// }
 
-function writeFileRelative(relativePath, data) {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(relativePath, data, 'utf8', (error) => {
-            if (error) {
-                reject(`Error reading file: ${error.message}`);
-            } else {
-                resolve();
-            }
-        });
-    });
-}
+// function writeFileRelative(relativePath, data) {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile(relativePath, data, 'utf8', (error) => {
+//             if (error) {
+//                 reject(`Error reading file: ${error.message}`);
+//             } else {
+//                 resolve();
+//             }
+//         });
+//     });
+// }
 
-function convertCoverageDataToLineArray(data) {
-    return data.split('\r\n');
-}
+// function convertCoverageDataToLineArray(data) {
+//     return data.split('\r\n');
+// }
 
-function normalizeCoverageDataLines(dataLines) {
-    if (dataLines.length <= 2) {
-        return [];
-    }
-    return dataLines.slice(1, dataLines.length - 1);
-}
+// function normalizeCoverageDataLines(dataLines) {
+//     if (dataLines.length <= 2) {
+//         return [];
+//     }
+//     return dataLines.slice(1, dataLines.length - 1);
+// }
 
-function mergeNormalizedCoverageDataLines(allDataLines) {
-    return allDataLines.flat();
-}
+// function mergeNormalizedCoverageDataLines(allDataLines) {
+//     return allDataLines.flat();
+// }
 
-function finalizeCoverageDataLines(dataLines) {
-    if (dataLines.length === 0) {
-        return ['TN:', 'end_of_record'];
-    }
-    return ['TN:', ...dataLines, 'end_of_record'];
-}
+// function finalizeCoverageDataLines(dataLines) {
+//     if (dataLines.length === 0) {
+//         return ['TN:', 'end_of_record'];
+//     }
+//     return ['TN:', ...dataLines, 'end_of_record'];
+// }
 
-function convertCoverageDataLinestoData(dataLines) {
-    return dataLines.join('\r\n');
-}
+// function convertCoverageDataLinestoData(dataLines) {
+//     return dataLines.join('\r\n');
+// }
 
 
-async function main() {
-    const coveragetFolderRelativePath = process.argv[2] ?? './coverage';
-    const lcovPaths = await globby(`${coveragetFolderRelativePath}/**/lcov.info`);
+// async function main() {
+//     const coveragetFolderRelativePath = process.argv[2] ?? './coverage';
+//     const lcovPaths = await globby(`${coveragetFolderRelativePath}/**/lcov.info`);
 
-    const coverageDatum = await Promise.all(lcovPaths.map(lcovPath => readFileRelative(lcovPath)))
+//     const coverageDatum = await Promise.all(lcovPaths.map(lcovPath => readFileRelative(lcovPath)))
 
-    const coverageDataLines = coverageDatum.map(coverageData => normalizeCoverageDataLines(convertCoverageDataToLineArray(coverageData)))
-    const mergedCoverageDataLines = mergeNormalizedCoverageDataLines(coverageDataLines);
-    const finalizedCoverageDataLines = finalizeCoverageDataLines(mergedCoverageDataLines);
-    const finalCoverageData = convertCoverageDataLinestoData(finalizedCoverageDataLines);
+//     const coverageDataLines = coverageDatum.map(coverageData => normalizeCoverageDataLines(convertCoverageDataToLineArray(coverageData)))
+//     const mergedCoverageDataLines = mergeNormalizedCoverageDataLines(coverageDataLines);
+//     const finalizedCoverageDataLines = finalizeCoverageDataLines(mergedCoverageDataLines);
+//     const finalCoverageData = convertCoverageDataLinestoData(finalizedCoverageDataLines);
 
-    writeFileRelative(`${coveragetFolderRelativePath}/lcov.info`, finalCoverageData).catch((error) => {
-        console.error(error);
-    })
-}
+//     writeFileRelative(`${coveragetFolderRelativePath}/lcov.info`, finalCoverageData).catch((error) => {
+//         console.error(error);
+//     })
+// }
 
-main();
+// main();
+
+console.log('this is working!');
