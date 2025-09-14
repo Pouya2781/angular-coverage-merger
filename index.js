9a7a4e69a18408f122a8d790382173b7b312c7f2
Pouya SadatAlhosseini <pouya.s2781@gmail.com>
2025-09-14 15:46:27 +0330

    fixed an edge case when coverage folder does not exist

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
-import fs from 'fs'
-import {globby} from 'globby'
-import {hideBin} from 'yargs/helpers'
-import yargs from 'yargs/yargs'
+import fs from 'fs';
+import path from 'path';
+import {globby} from 'globby';
+import {hideBin} from 'yargs/helpers';
+import yargs from 'yargs/yargs';
 
 function readFileRelative(relativePath) {
     return new Promise((resolve, reject) => {
@@ -18,63 +19,67 @@ function readFileRelative(relativePath) {
 
 function writeFileRelative(relativePath, data) {
     return new Promise((resolve, reject) => {
-        fs.writeFile(relativePath, data, 'utf8', (error) => {
-            if (error) {
-                reject(`Error reading file: ${error.message}`);
-            } else {
-                resolve();
-            }
+        fs.mkdir(path.dirname(relativePath), {recursive: true}, (err) => {
+            if (err) return reject(`Error creating folder: ${err.message}`);
+
+            fs.writeFile(relativePath, data, 'utf8', (error) => {
+                if (error) {
+                    reject(`Error writing file: ${error.message}`);
+                } else {
+                    resolve();
+                }
+            });
         });
     });
 }
 
 async function main() {
     const argv = yargs(hideBin(process.argv))
-    .option('directory', {
-        alias: 'd',
-        type: 'string',
-        description: 'Coverage directory path (by default will be ./coverage)',
-    })
-    .option('output', {
-        alias: 'o',
-        type: 'string',
-        description: 'Coverage output file path (by default will be lcov.info in coverage directory path)',
-    })
-    .option('verbose', {
-        alias: 'v',
-        type: 'boolean',
-        description: 'Enable verbose mode',
-    })
-    .help()
-    .argv;
+        .option('directory', {
+            alias: 'd',
+            type: 'string',
+            description: 'Coverage directory path (by default will be ./coverage)',
+        })
+        .option('output', {
+            alias: 'o',
+            type: 'string',
+            description: 'Coverage output file path (by default will be lcov.info in coverage directory path)',
+        })
+        .option('verbose', {
+            alias: 'v',
+            type: 'boolean',
+            description: 'Enable verbose mode',
+        })
+        .help().argv;
 
     const verbose = argv.verbose;
     const coveragetFolderRelativePath = argv.directory ?? './coverage';
     const coveragetOutputRelativePath = argv.output ?? `${coveragetFolderRelativePath}/lcov.info`;
 
     const lcovPaths = await globby(`${coveragetFolderRelativePath}/**/**/lcov.info`);
-    const lcovValidPaths = lcovPaths.filter(lcovPath => lcovPath !== `${coveragetFolderRelativePath}/lcov.info`);
+    const lcovValidPaths = lcovPaths.filter((lcovPath) => lcovPath !== `${coveragetFolderRelativePath}/lcov.info`);
 
     if (verbose) {
         if (lcovValidPaths.length === 0) {
-            console.log(`No coverage file found in "${coveragetFolderRelativePath}"!`)
+            console.log(`No coverage file found in "${coveragetFolderRelativePath}"!`);
         } else {
-            console.log(`Found ${lcovValidPaths.length} coverage files in "${coveragetFolderRelativePath}":`)
-            lcovValidPaths.forEach((lcovValidPath, index) => console.log(`${index + 1}. "${lcovValidPath}"`))
+            console.log(`Found ${lcovValidPaths.length} coverage files in "${coveragetFolderRelativePath}":`);
+            lcovValidPaths.forEach((lcovValidPath, index) => console.log(`${index + 1}. "${lcovValidPath}"`));
         }
-    
     }
 
-    const coverageDatum = await Promise.all(lcovValidPaths.map(lcovPath => readFileRelative(lcovPath)))
+    const coverageDatum = await Promise.all(lcovValidPaths.map((lcovPath) => readFileRelative(lcovPath)));
     const mergedData = coverageDatum.join('\n');
 
-    writeFileRelative(coveragetOutputRelativePath, mergedData).then(() => {
-        if (verbose) {
-            console.log(`Output coverage file has been succesfully stored in "${coveragetOutputRelativePath}"!`)
-        }
-    }).catch((error) => {
-        console.error(error);
-    })
+    writeFileRelative(coveragetOutputRelativePath, mergedData)
+        .then(() => {
+            if (verbose) {
+                console.log(`Output coverage file has been successfully stored in "${coveragetOutputRelativePath}"!`);
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 }
 
 main();
